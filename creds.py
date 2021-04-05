7f55997854fa7329f14f14d0cf58035847777b9d
ankiyzxi25 <74183863+ankiyzxi25@users.noreply.github.com>
2021-04-05 07:39:22 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,5 +1,5 @@
 import os
 class config():
-	TG_BOT_TOKEN=os.environ['TG_BOT_TOKEN']
-	API_ID=os.environ['API_ID']
-	API_HASH=os.environ['API_HASH']
+	TG_BOT_TOKEN=os.environ['1603330214:AAEgM98u_h3jNYmF-tC37gw0djKdvqCtW50']
+	API_ID=os.environ['1368264']
+	API_HASH=os.environ['e71fe0dcd8585c41201187161a42b904']
